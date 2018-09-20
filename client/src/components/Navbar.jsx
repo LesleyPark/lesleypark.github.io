5b1c4b45dbf015d57c704812d6c5c97a0433607e
LesleyPark <lesparkley@gmail.com>
2018-09-20 16:31:09 -0700

    "Added resume, navbar, and links to Github and LinkedIn"

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from '../stylesheets/Navbar.css';
+
+class Navbar extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <div className="row">
+          <div class="col-sm">
+            <h5>Résumé</h5>
+          </div>
+          <div class="col-sm">
+              <h5>Connect</h5>
+              <a href="https://github.com/LesleyPark" target="_blank">
+                <img className={styles.resize} src="https://s3-us-west-1.amazonaws.com/lppersonalwebsite/gh.png" />&nbsp;
+              </a>
+              <a href="https://www.linkedin.com/in/lesleypark/" target="_blank">
+                <img className={styles.resize} src="https://s3-us-west-1.amazonaws.com/lppersonalwebsite/li.png" />
+              </a>
+          </div>
+          <div class="col-sm">
+            <h5>Contact</h5>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
---
+++ client/src/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './components/Navbar.jsx';
+import Resume from './components/Resume.jsx';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render () {
+    return (
+      <div>
+        <div className="title"><h1>L E S L E Y&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;P A R K</h1>
+          <div className="italics">
+            <h5>Software Engineer</h5>
+          </div>
+          <hr />
+          <Navbar />
+          <Resume />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
