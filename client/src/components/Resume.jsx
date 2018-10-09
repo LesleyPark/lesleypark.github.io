8eedd9729f2ee1a6d98b2dfb34fefe8a7c185dd1
LesleyPark <lesparkley@gmail.com>
2018-10-08 19:08:49 -0700

    Animated resume sections

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from '../stylesheets/Resume.css'
+import ScrollAnimation from 'react-animate-on-scroll';
+import styles from '../stylesheets/Resume.css';
 
 class Resume extends React.Component {
   constructor() {
@@ -10,15 +11,20 @@ class Resume extends React.Component {
   render () {
     return (
       <div className={styles.resume}>
+        <ScrollAnimation animateIn='fadeIn' animateOnce='true'>
         <span className={styles.headers}><h6>TECHNICAL SKILLS</h6></span>
         Front End | Javascript (ES5/ES6), React, AngularJS, HTML5, CSS3
         <br />Back End | Node.js, Express, PostgreSQL, MySQL, Sequelize, MongoDB, Mongoose, Ruby, Rails, C
         <br />Developer Tools | Git, npm, Webpack, Babel, Agile
         <br /><br />
+        </ScrollAnimation>
+        <ScrollAnimation animateIn='fadeIn' animateOnce='true'>
         <span className={styles.headers}><h6>EDUCATION</h6></span>
         Hack Reactor | Advanced Software Engineering Immersive			      	June 2018 - September 2018
         <br />University of Southern California | B.S. Chemical Engineering			      August 2005 - May 2009
         <br /><br />
+        </ScrollAnimation>
+        <ScrollAnimation animateIn='fadeIn' animateOnce='true'>
         <span className={styles.headers}><h6>PROFESSIONAL EXPERIENCE</h6></span>
         Associate Field Applications Engineer | Semtech Corporation			         March 2010 - May 2018
         <ul><li>Drove the automation of the New Product Idea program which resulted in the creation of over 55 new parts that have brought in over $20M in revenue cumulatively</li>
@@ -27,6 +33,8 @@ class Resume extends React.Component {
         Marketing Consultant | Framed [YCW14, Acquired by Square]		           September 2014 - March 2016
         <ul><li>Developed copy and content relating to core marketing strategies for B2B SaaS, analytics, data science, and customer success which accounted for ~30% total website traffic</li>
         <li>Utilized analytics tools (Google Analytics, Kissmetrics) to quantify customer behaviors and built marketing automation sequences resulting in an average 50% MoM revenue growth from July 2015 to March 2016</li></ul>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn='fadeIn' animateOnce='true'>
         <span className={styles.headers}><h6>SOFTWARE ENGINEERING APPLICATIONS</h6></span>
         Airbnb Listing Replication | Javascript (ES5/ES6), React, HTML5, CSS3, PostgreSQL, AWS EC2
         Collaborative application that mirrors the design and functionality of Airbnb’s front end
@@ -37,6 +45,7 @@ class Resume extends React.Component {
         Created wireframes and designed application front end
         <ul><li>Built legal piece movement validation and game status logic (e.g. check or stalemate)</li>
         <li>Used RuboCop gem to enforce the team’s conformity to the Ruby style guide and CodeShip as a platform for continuous integration and deployment</li></ul>
+        </ScrollAnimation>
       </div>
     )
   }
